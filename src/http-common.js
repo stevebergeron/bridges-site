f1cda34856f9dd58b7e72dbd5e1c85231eded388
Steve Bergeron <steve.bergeron1@gmail.com>
2020-07-23 22:26:10 -0400

    Let's try this

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,16 @@
 import axios from "axios";
+
+var baseURL;
 console.log(process.env);
+
+if (process.env.NODE_ENV === 'development') {
+  baseURL = "http://localhost:8080/api"
+} else {
+  baseURL = "https://vtbridges-api.herokuapp.com//api"
+};
+
 export default axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: baseURL,
   headers: {
     "Content-type": "application/json"
   }
