3354e59899e1b27ecf5c53ab621003ef4afacd6c
Steve Bergeron <steve.bergeron1@gmail.com>
2020-07-23 22:54:22 -0400

    Putting Service Worker back in

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import * as serviceWorker from "./serviceWorker";
 
 import App from "./App";
 
@@ -11,3 +12,4 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
+serviceWorker.unregister();
