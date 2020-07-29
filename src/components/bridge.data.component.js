6a143b527064328c96ff636484b3ea4c77cd87ba
Steve Bergeron <steve.bergeron1@gmail.com>
2020-07-28 23:39:03 -0400

    Slides are working ... need to tweak captions

--- src/components/bridge.data.component.js
+++ src/components/bridge.data.component.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import DataService from "../services/data.service";
-import { Transformation, CloudinaryContext, Image } from 'cloudinary-react';
-const axios = require('axios');
+import { Transformation, Image } from 'cloudinary-react';
+import { Carousel, CarouselItem } from "react-bootstrap";
 
 export default class BridgeDataDisplay extends Component {
   constructor(props) {
@@ -16,54 +16,48 @@ export default class BridgeDataDisplay extends Component {
   };
 
   render() {
-      DataService.findByWGN(this.props.wgn)
-        .then(response => {
-          this.setState({
-            activeBridge: response.data.bridges[0],
-            activeArticles: response.data.articles,
-            activePhotos: response.data.photos
-          });
-        })
-        .catch(e => { console.log(e); });
+    DataService.findByWGN(this.props.wgn)
+      .then(response => {
+        this.setState({
+          activeBridge: response.data.bridges[0],
+          activeArticles: response.data.articles,
+          activePhotos: response.data.photos
+        });
+      })
+      .catch(e => { console.log(e); });
 
     return (
       <div>
         {this.state.activeBridge ?
-        (
-        <div>
-          <h3 className="text-light">{this.state.activeBridge.name}</h3>
+          (
+            <div>
+              <h3 className="text-light">{this.state.activeBridge.name}</h3>
               <div>
-              <div className="gallery">
-                  <CloudinaryContext cloudName="hjugvucxs">
-                    <div className="responsive">
-                      {this.state.activePhotos.map(data => {
-                        return (
-                          <div className="responsive" key={data.id}>
 
-                            <Image publicId={data.link}>
-                            <Transformation
-                                                        crop="scale"
-                                                        width="300"
-                                                        height="200"
-                                                        dpr="auto"
-                                                        responsive_placeholder="blank"
-                                                    />
-                            </Image>
-                            
-                          </div>
-
-                        )
-                      })
-                      }
-                    </div>
-                  </CloudinaryContext>
-                </div>
+                <Carousel id="photo-carousel" >
+                  {this.state.activePhotos.map(data => {
+                    return (
+                      <CarouselItem key={data.id} className={"d-block w-100 " + data.id === 1 ? "active" : ""}>
+                        <Carousel.Caption>
+                          <span className="bg-dark">Slide Caption</span>
+                        </Carousel.Caption>
+                        <Image cloudName="hjugvucxs" publicId={data.link} className="cld-responsive d-block w-100">
+                          <Transformation
+                            crop="scale"
+                            width="800"
+                          />
+                        </Image>
+                      </CarouselItem>
+                    )
+                  })
+                  }
+                </Carousel>
               </div>
             </div>
-        ) :
-        ( 
-           <h3 className="text-light">This will be the intro...</h3>
-        ) }
+          ) :
+          (
+            <h3 className="text-light">This will be the intro...</h3>
+          )}
       </div>
     )
   }
