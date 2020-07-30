2313eee642de393847d148c08837b48840a5ac7b
Steve Bergeron <steve.bergeron1@gmail.com>
2020-07-30 00:53:51 -0400

    Done, other than making collapsible menu

--- src/App.js
+++ src/App.js
@@ -6,26 +6,28 @@
  * July 2020
  */
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Main from "./components/main.component";
 import { Navbar, NavbarBrand, Container } from "react-bootstrap";
+import './css/dashboard.css'
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <Container>
-          <Navbar bg="dark" variant="dark" sticky="top">
-            <NavbarBrand href="/bridges" >
-              <h3 className="text-warning">Vermont Covered Bridges (on Two Wheels!)</h3>
-            </NavbarBrand>
-          </Navbar>
-          <Container>
-            <Route exact path={["/", "/bridges"]} component={Main} />
-          </Container>
-        </Container>
-      </Router>
+      <Container fluid>
+        <Navbar bg="dark" variant="dark" sticky="top">
+          <NavbarBrand href="/bridges" className="col-md-6">
+            <h3 className="text-warning">Vermont Covered Bridges on Two Wheels</h3>
+            
+          </NavbarBrand>
+          <div>
+            <h6 className="text-warning text-right">
+              <em>A photographic journey to Vermont's Covered Bridges</em>
+            </h6>
+          </div>
+        </Navbar>
+        <Main />
+      </Container>
     );
   }
 }
